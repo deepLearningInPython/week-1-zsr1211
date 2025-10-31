67e60b21243cccc9368ef761421e002dea4e681b
zsr1211 <Zshiru1211@gmail.com>
2025-10-31 15:57:47 +0000

    Finished tasks 1-3 and certificate

--- tasks.py
+++ tasks.py
@@ -13,7 +13,11 @@ import numpy
 # Your code here:
 # -----------------------------------------------
 
-def step
+def step(num):
+  if num > 0:
+    return 1
+  else:
+    return -1
 
 
 # -----------------------------------------------
@@ -28,7 +32,9 @@ def step
 
 # Your code here:
 # -----------------------------------------------
-def ReLu
+def ReLu(array, cutoff=0):
+  new_array = numpy.maximum(array, cutoff)
+  return new_array
 
 
 # -----------------------------------------------
@@ -44,7 +50,10 @@ def ReLu
 # Your code here:
 # -----------------------------------------------
 
-def neural_net_layer
+def neural_net_layer(T, O):
+  result = numpy.dot(T, O)
+  actived = ReLu(result)
+  return actived
 
 
 # ------------------------------------------
